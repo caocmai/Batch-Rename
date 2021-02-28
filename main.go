c339a667e4002a21194805111bb108ff28743ea1
Cao Mai <caomai@rocketmail.com>
2021-02-28 10:44:20 -0800

    Add multiple flags to customize output folder and file names

--- main.go
+++ main.go
@@ -10,12 +10,12 @@ import (
 	"strings"
 )
 
-func renameAndMoveFiles(OUTPUTNAME string, fileType string) {
+func renameAndMoveFiles(fileType string, outputName string, newfileName string) {
 
-	_, err := os.Stat(OUTPUTNAME)
+	_, err := os.Stat(outputName)
 
 	if os.IsNotExist(err) {
-		errDir := os.MkdirAll(OUTPUTNAME, 0755)
+		errDir := os.MkdirAll(outputName, 0755)
 		if errDir != nil {
 			log.Fatal(err)
 		}
@@ -39,10 +39,10 @@ func renameAndMoveFiles(OUTPUTNAME string, fileType string) {
 			fileNameWithoutExtension := strings.Split(file.Name(), fileType)[0]
 			fmt.Println(fileNameWithoutExtension)
 
-			os.Rename(file.Name(), strconv.Itoa(counter)+"_new"+fileType)
-			oldDir := dir + "/" + strconv.Itoa(counter) + "_new" + fileType
-			finalDir := dir + "/" + OUTPUTNAME + "/" + strconv.Itoa(counter) + "_new" + fileType
-			os.Rename(oldDir, finalDir)
+			os.Rename(file.Name(), strconv.Itoa(counter)+"_"+newfileName+fileType)
+			oldFileDir := dir + "/" + strconv.Itoa(counter) + "_" + newfileName + fileType
+			finalFileDir := dir + "/" + outputName + "/" + strconv.Itoa(counter) + "_" + newfileName + fileType
+			os.Rename(oldFileDir, finalFileDir)
 
 			counter++
 
@@ -52,10 +52,20 @@ func renameAndMoveFiles(OUTPUTNAME string, fileType string) {
 }
 
 func main() {
-	OUTPUTNAME := "test"
-	var typeFlag string
-	flag.StringVar(&typeFlag, "filetype", "00000", "Enter type of file do you want to rename")
+
+	var config struct { // [1]
+		filetypeName     string
+		outputFolderName string
+		outputFileName   string
+	}
+
+	flag.StringVar(&config.filetypeName, "filetype", "0000000", "Enter filetype you want to rename")
+	flag.StringVar(&config.outputFolderName, "outputFolderName", "test", "Enter file name to store renamed files")
+	flag.StringVar(&config.outputFileName, "newFileName", "renamed_default", "Rename files as this")
 	flag.Parse()
 
-	renameAndMoveFiles(OUTPUTNAME, typeFlag)
+	fmt.Println(config.filetypeName)
+	fmt.Println(config.outputFolderName)
+
+	renameAndMoveFiles(config.filetypeName, config.outputFolderName, config.outputFileName)
 }
