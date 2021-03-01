91bdf461b99f02d82aaffe15fcb42064d67996d0
Cao Mai <caomai@rocketmail.com>
2021-02-28 21:11:05 -0800

    Add comments and rearrange code

--- main.go
+++ main.go
@@ -11,48 +11,6 @@ import (
 	"strings"
 )
 
-func renameAndMoveFiles(fileType string, outputFolderName string, newfileName string, inputFolder string) {
-
-	createOutputFolder(outputFolderName)
-
-	// Get the current working directory
-	workingDir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Read the current working directory + inputFolder name for files
-	specifiedWorkingDir := workingDir + "/" + inputFolder
-	files := getFilesFromDir(inputFolder)
-
-	counter := 0
-
-	// Loop through all files in specified folder
-	for _, file := range files {
-		if strings.Contains(file.Name(), fileType) {
-			// fileNameWithoutExtension := strings.Split(file.Name(), fileType)[0]
-			// fmt.Println(fileNameWithoutExtension)
-
-			// Rename and move file
-			finalFileDir := workingDir + "/" + outputFolderName + "/" + strconv.Itoa(counter) + "_" + newfileName + fileType
-
-			err := os.Rename(filepath.Join(specifiedWorkingDir, file.Name()), finalFileDir)
-
-			if err != nil {
-				log.Fatal(err)
-			}
-			counter++
-		}
-	}
-
-	if counter > 0 {
-		fmt.Println(counter, fileType, "was renamed")
-	}
-
-	getFilesFromDir(inputFolder)
-
-}
-
 func createOutputFolder(outputFolderName string) {
 	// Checks if output file name already exsits
 	_, err := os.Stat(outputFolderName)
@@ -87,6 +45,7 @@ func getFilesFromDir(inputFolder string) (files []os.FileInfo) {
 		isEmpty = true
 	}
 
+	// If there are no files in folder then delete that folder
 	if isEmpty {
 		os.Remove(specifiedWorkingDir)
 	}
@@ -95,8 +54,52 @@ func getFilesFromDir(inputFolder string) (files []os.FileInfo) {
 
 }
 
+func renameAndMoveFiles(fileType string, outputFolderName string, newfileName string, inputFolder string) {
+
+	createOutputFolder(outputFolderName)
+
+	// Get the current working directory
+	workingDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Read the current working directory + inputFolder name for files
+	specifiedWorkingDir := workingDir + "/" + inputFolder
+	files := getFilesFromDir(inputFolder)
+
+	counter := 0
+
+	// Loop through all files in specified folder
+	for _, file := range files {
+		if strings.Contains(file.Name(), fileType) {
+			// fileNameWithoutExtension := strings.Split(file.Name(), fileType)[0]
+			// fmt.Println(fileNameWithoutExtension)
+
+			// Rename and move file
+			finalFileDir := workingDir + "/" + outputFolderName + "/" + strconv.Itoa(counter) + "_" + newfileName + fileType
+
+			err := os.Rename(filepath.Join(specifiedWorkingDir, file.Name()), finalFileDir)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+			counter++
+		}
+	}
+
+	// Logs out how many files were renamed
+	if counter > 0 {
+		fmt.Println(counter, fileType, "was renamed")
+	}
+
+	getFilesFromDir(inputFolder)
+
+}
+
 func main() {
 
+	// config stuct to store flags
 	var config struct {
 		inputFolderName  string
 		filetypeName     string
